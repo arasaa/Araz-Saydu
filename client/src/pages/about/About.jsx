4ef24c6b62f4b0fa9580d990c1c6ae974bfa63ed
araz saydu <arazsaydu@gmail.com>
2022-04-21 14:34:56 +0200

    first design of my about page

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './about.css'
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about-container">
+      
+    <div className="div-about">
+      <figure className="figureBoth front"></figure>
+      <figure className="figureBoth back">
+      </figure>
+    </div>
+
+    <p>About</p><br/>
+      <p>Abouthhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</p><br/>
+      <p>About</p><br/>
+      <p>About</p><br/>
+      <p>About</p><br/>
+      <p>About</p><br/>
+
+
+    </div>
   )
 }
 
