dd23d75b0840af3269dbd60ca50c7fa0a91e445e
araz saydu <arazsaydu@gmail.com>
2022-04-22 19:47:48 +0200

    just added about text

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -12,13 +12,29 @@ const About = () => {
       </figure>
     </div>
 
-    <p>About</p><br/>
-      <p>Abouthhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</p><br/>
-      <p>About</p><br/>
-      <p>About</p><br/>
-      <p>About</p><br/>
-      <p>About</p><br/>
+<section className="about-section">
+    <p>
+      Araz Saydu,
+      was born in Syria, Qamishli.
+      It all started when i wanted to create a portfolio for me and write there  some thoughts.
+      So i built one on Google Blogger and tried to improve it.
+      i should learn HTML and CSS to that.
+      because there was no internet permanently I did'nt learn a lot.
+      then i found out  I should learn something mre advanced so I began to learn PHP after
+       a while I come to Germany and started to learn German so I did!
+       I saw I need to learn something else so I began to learn JAVA on YOUTUBE and UDEMY and
+       visited a course at ReDI-School. I did'nt find myself excited to learn JAVA.
+       I searched for something makes me excited and I would love to learn and do it I chose JAVASCRIPT.
+       I applied for a Full Stack Web developer course at DCI - Digital Career Institute gGmbH.
+       I did the course successfully.
+       I did the final project and was satisfied with myself.
+       I learned a lot and I know now I should keeping learning.
+       This field is the field for people who are excited to learn and learn every day and I'm like that.
 
+
+    </p>
+     
+      </section>
 
     </div>
   )
